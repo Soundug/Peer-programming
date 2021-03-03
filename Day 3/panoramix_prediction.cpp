1fecd887ee81113a64e5d3a29e87e06c7993ef4b
Soundarya G <soundaryagsk@gmail.com>
2021-03-03 16:35:47 +0530

    Create panoramix_prediction.cpp

---
+++ Day 3/panoramix_prediction.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkprime(int n)
+{
+    for(int i = 2; i <= sqrt(n); i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    int n,m;
+    cin >> n >> m;
+    
+    int n1 = n + 1;
+    while(!checkprime(n1)) {
+        n1 += 1;
+    }
+    if(n1 == m) cout << "YES";
+    else cout << "NO";
+    cout << "\n";
+    
+    return 0;
+}
