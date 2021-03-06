652a8e4f12fa8307d83f8b2b5ef9861e4da7e2ab
Soundarya G <soundaryagsk@gmail.com>
2021-03-06 21:29:24 +0530

    Update soft_drinking.cpp

--- Day 6/soft_drinking.cpp
+++ Day 6/soft_drinking.cpp
@@ -2,23 +2,21 @@
 #include<algorithm>
 using namespace std;
 
-bool compare(int a, int b){
-    return (a < b);
-}
-
 int main()
 {
     int n,k,l,c,d,p,nl,np;
     
     cin >> n >> k >> l >> c >> d >> p >> nl >> np;
     
-    int toasts = (k*l)/nl;
+    int drink = (k*l)/nl;
     
     int limes = c*d;
     
     int salt = p/np;
     
-    cout << min({toasts, limes, salt}, compare) << "\n";
+    int toast = min(drink, min(limes, salt));
+    
+    cout << toast/n << endl;
     
     return 0;
 }
