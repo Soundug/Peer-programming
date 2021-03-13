4f06eb98a2200aa34a9650f0ff79c9c3ca16bdf2
Soundarya G <soundaryagsk@gmail.com>
2021-03-13 23:26:28 +0530

    Create Puzzles.cpp

---
+++ Day 13/Puzzles.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	int f[m];
+	for (int i = 0; i < m; i++) {
+		cin >> f[i];
+	}
+	sort(f, f + m);
+	int ans = INT_MAX;
+	// 5 7 10 10 12 22
+	// 0 1  2  3  4  5
+	for (int i = 0; i < (m - n) + 1; i++) {
+		ans = min(ans, f[i + n - 1] - f[i]);
+	}
+	cout << ans << '\n';
+	return 0;
+}
