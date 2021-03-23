f312b676d79331fad01285b138f5dad05af10611
Soundarya G <soundaryagsk@gmail.com>
2021-03-23 12:49:29 +0530

    Create Adding_digits.cpp

---
+++ Day 22/Adding_digits.cpp
@@ -0,0 +1,39 @@
+/*
+*	User: Soundu
+*	Problem: CodeForces 260A - Adding Digits
+* http://codeforces.com/problemset/problem/260/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using Number = long long;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	Number a, b, n;
+	cin >> a >> b >> n;
+
+	int nextDigit = -1;
+
+	a *= 10;
+
+	for (int i = 0; i <= 9; ++i)
+	{
+		if ((a + i) % b == 0)
+		{
+			nextDigit = i;
+			break;
+		}
+	}
+
+	if (nextDigit != -1)
+		cout << a + nextDigit << setw(n) << setfill('0') << "\n";
+	else
+		cout << "-1\n";
+
+	return 0;
+}
