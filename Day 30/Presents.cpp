8703e0ffee4487766b54158b6a6a9a42b5a5f8a9
Soundarya G <soundaryagsk@gmail.com>
2021-04-02 23:15:10 +0530

    Create Presents.cpp

---
+++ Day 30/Presents.cpp
@@ -0,0 +1,23 @@
+/* User - Soundu
+* Problem - http://codeforces.com/problemset/problem/136/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int a[100], n, p;
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		cin >> p;
+		a[p] = i;
+	}
+	for(int i = 1; i <= n; i++){
+		cout << a[i] << " ";
+	}
+	cout << "\n";
+	return 0;
+}
