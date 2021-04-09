a798634ce377c4c1259ad8c76744903c93a8b769
Soundarya G <soundaryagsk@gmail.com>
2021-04-09 23:43:12 +0530

    Create Lefthanders_and_righthanders.cpp

---
+++ Day 37/Lefthanders_and_righthanders.cpp
@@ -0,0 +1,26 @@
+/* User - Soundu
+* Problem - http://codeforces.com/problemset/problem/234/A
+*/ 
+
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+const int N = 100;
+
+int n;
+char type[N + 2];
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    scanf("%d%s", &n, type + 1);
+    for (int i = 1; i <= n >> 1; ++ i) {
+        if (type[i] == 'R' && type[i + (n >> 1)] == 'L') {
+            printf("%d %d\n", i + (n >> 1), i);
+        } else {
+            printf("%d %d\n", i, i + (n >> 1));
+        }
+    }
+    return 0;
+}
