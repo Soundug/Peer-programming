3ab02604a19b4a7dad04a04a1674d3e4853b4cf3
Soundarya G <soundaryagsk@gmail.com>
2021-04-09 23:49:28 +0530

    Create Cows_and_primitive_roots.cpp

---
+++ Day 37/Cows_and_primitive_roots.cpp
@@ -0,0 +1,38 @@
+/* User - Soundu
+* Problem - http://codeforces.com/problemset/problem/284/A
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int p,root,count;
+    
+    cin >>p;
+    root = p-1;
+    count = root;
+    for(int i=2; i*i<=root; i++){
+        if(root%i == 0){
+            do{
+                root /= i;
+            }while(root%i == 0);
+            count = count/i*(i-1);
+        }
+    }
+    if(root != 1){count = count/root*(root-1);}
+    
+    cout << count <<endl;
+
+return 0;
+}
